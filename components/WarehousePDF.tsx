725a55a039fe5f3f01ac9da17e36bad373cdc2ea
mnowak2504 <m.nowak@makconsulting.pl>
2026-01-23 10:54:57 +0100

    Fix Polish characters in warehouse PDF - change font from Times-Roman to Helvetica

--- components/WarehousePDF.tsx
+++ components/WarehousePDF.tsx
@@ -6,7 +6,7 @@ const styles = StyleSheet.create({
   page: {
     padding: 40,
     fontSize: 10,
-    fontFamily: 'Times-Roman',
+    fontFamily: 'Helvetica',
     backgroundColor: '#ffffff',
   },
   header: {
